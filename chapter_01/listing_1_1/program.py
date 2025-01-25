fa17629e0815d5c5a74649a4dcb87c4d48de99db
aleksey <aleks.twin@gmail.com>
2025-01-25 19:44:37 +0300

    Initial setup and listing 1.1

---
+++ chapter_01/listing_1_1/program.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get('https://www.example.com')
+
+#print( response.status_code )
+
+items = response.headers.items()
+
+headers = [f'{key}: {header}' for key, header in items]
+
+formatted_headers = '\n'.join(headers)
+
+with open('headers.txt', 'w') as file:
+    file.write(formatted_headers)
