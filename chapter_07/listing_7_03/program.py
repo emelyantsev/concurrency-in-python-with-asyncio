3595f4f5fee9a5c20c3a8971a02ede9c99c6972a
Aleksey Emelyantsev <aleks.twin@gmail.com>
2025-03-02 11:42:45 +0300

    add listings 7_3 - 7_7

---
+++ chapter_07/listing_7_03/program.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+
+url = 'https://www.example.com'
+
+print( get_status_code(url) )
+print( get_status_code(url) )
